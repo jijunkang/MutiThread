a82a86ecf6b321509b324a50b89cd89001ad26a9
jijunkang <1148862830@qq.com>
2022-08-15 10:55:07 +0800

    1

---
+++ src/main/java/com/example/mutithread/MutiThreadApplication.java
@@ -0,0 +1,13 @@
+package com.example.mutithread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutiThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MutiThreadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mutithread/MutiThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mutithread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutiThreadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
